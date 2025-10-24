c0bb0a9f1ab9ef00a01e568b70c9006bd3800ecb
IliaDiv <divinskyilia@gmail.com>
2025-10-24 16:26:16 +0300

    fix #11

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set default API_URL if not provided
-export API_URL=${API_URL:-http://backend:5000}
+export API_URL=${API_URL:-http://localhost}
 
 # Debug: show the API_URL value
 echo "API_URL is set to: $API_URL"
